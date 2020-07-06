ff667c4e2f5eb2c85ec098b1680adc9b97bb2a4e
ryota <ryota.fukumoto@stv-tech.co.jp>
2020-07-06 16:53:00 +0900

    ナビゲーションバーの色ステータスバーの色　文字色変更

---
+++ lib/const.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+const Color whitew = Color(0xFFFFFFFF);
+
+const MaterialColor orangeColorShades = MaterialColor(
+  0xFFEC8E35,
+  <int, Color>{
+    50: Color(0xFFEC8E35),
+    100: Color(0xFFEC8E35),
+    200: Color(0xFFEC8E35),
+    300: Color(0xFFEC8E35),
+    400: Color(0xFFEC8E35),
+    500: Color(0xFFEC8E35),
+    600: Color(0xFFEC8E35),
+    700: Color(0xFFEC8E35),
+    800: Color(0xFFEC8E35),
+    900: Color(0xFFEC8E35),
+  },
+);
+
+const MaterialColor whiteColorShades = MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFFFFFFF),
+    300: Color(0xFFFFFFFF),
+    400: Color(0xFFFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xFFFFFFFF),
+    700: Color(0xFFFFFFFF),
+    800: Color(0xFFFFFFFF),
+    900: Color(0xFFFFFFFF),
+  },
+);
